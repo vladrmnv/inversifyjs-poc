8e5a6618a5dc7618933a12bcd821a7de24ac133d
Vlad R <vromanov@nestwealth.com>
2018-11-22 17:15:30 -0500

    added index.ts

---
+++ index.ts
@@ -0,0 +1,12 @@
+async function main() {
+  console.log('works');
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
