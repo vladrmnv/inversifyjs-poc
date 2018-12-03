2beb9025ba8685fff2761ac8a6b1b0823929ee45
Vlad R <vromanov@nestwealth.com>
2018-12-03 14:38:59 -0500

    added use for notifications service

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import 'reflect-metadata';
 import { loadDiContainer } from './app/root.container';
-import { ReportingService, IReportingService } from './reporting/reporting.service';
-import { TYPES } from './reporting/reporting.module';
+import { IReportingService } from './reporting/reporting.service';
+import { TYPES as ReportingTypes } from './reporting/reporting.module';
+import { TYPES as NotificationsTypes } from './notifications/notifications.module';
+import { INotificationsService } from './notifications/notifications.service';
 
 async function main() {
   const rootContainer = loadDiContainer();
-  const reportingService = rootContainer.get<IReportingService>(TYPES.ReportingService);
+  const reportingService = rootContainer.get<IReportingService>(ReportingTypes.ReportingService);
   const report = reportingService.createAccountingReport();
-  console.log(report);
+  console.log('Report:', report);
+
+  const notificationService = rootContainer.get<INotificationsService>(NotificationsTypes.NotificationsService);
+  const updates = notificationService.getUpdates();
+  console.log('Updates:', updates);
 }
 
 main()
